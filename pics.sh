0d9710e6cd62b051520917c9bb63d1cd8a025a79
Daniel V Mathew <danielvmathew27@gmail.com>
2025-10-21 17:52:52 +0530

    Compressed more pngs

---
+++ pics.sh
@@ -0,0 +1,32 @@
+compress() {
+    magick "$1" -colors 64 out.png
+    cp out.png "$1"
+}
+
+compress ./chapter_06/section_02/pics/side_hatch_right.png
+compress ./chapter_06/section_02/pics/top_hatch_right.png
+compress ./chapter_05/section_07/pics/ext_inlets_right.png
+compress ./chapter_05/section_07/pics/ext_right.png
+compress ./chapter_05/section_02/pics/ded_cooler_right.png
+compress ./chapter_01/section_05/pics/hume_res_left.png
+compress ./chapter_01/section_05/pics/hatch_both_right.png
+compress ./chapter_01/section_05/pics/ext_both_right.png
+compress ./chapter_01/section_05/pics/water_res_left.png
+compress ./chapter_01/section_05/pics/inc_area_left.png
+compress ./chapter_01/section_05/pics/elec_bay_right.png
+compress ./chapter_04/section_03/pics/elec_bay_right.png
+compress ./chapter_04/section_02/pics/inc_area_right.png
+compress ./chapter_04/section_02/pics/ext_right.png
+compress ./chapter_04/section_02/pics/side_hatch_right.png
+compress ./chapter_04/section_02/pics/top_hatch_right.png
+compress ./chapter_04/section_01/pics/back_side_panels.png
+compress ./chapter_04/section_01/pics/frame_cad.png
+compress ./chapter_04/section_01/pics/front_base.png
+compress ./chapter_04/section_01/pics/back_side_panel.png
+compress ./chapter_04/section_01/pics/back_base.png
+compress ./chapter_04/section_01/pics/elec_bay.png
+compress ./chapter_04/section_01/pics/front_panel.png
+compress ./chapter_04/section_01/pics/front_back_panels.png
+compress ./chapter_04/section_01/pics/front_side_panels.png
+compress ./chapter_04/section_01/pics/back_side_hatch.png
+compress ./chapter_04/section_01/pics/front_side_panel.png
